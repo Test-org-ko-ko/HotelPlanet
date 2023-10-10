be40c24aabb8656166f93de3c6f2b70bbdbf8865
Winnie Lwin <wintnandar.wnl@gmail.com>
2023-10-09 20:34:50 -0500

    Create FooterComponent.tsx
    Footer component

---
+++ src/components/FooterComponent.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+const FooterComponent = (props: any) => {
+  return (
+    <div>
+      <footer className="footer">
+        <span className="text-muted">All Rights Reserved @Hotel Planet</span>
+      </footer>
+    </div>
+  );
+};
+
+export default FooterComponent;
